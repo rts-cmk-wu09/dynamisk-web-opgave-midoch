e4ecf9f51244c80811da0aff03862f5a8692f56b
mido <101448145+midoch@users.noreply.github.com>
2023-02-13 10:44:45 +0100

    added content

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  // slå dig løs her...
+  document.querySelector(".hero").innerHTML = `
+    <img src="${hero.image}" alt="hero image" />
+    <h1>${hero.headline}</h1>
+    <p>${hero.copy}</p>
+    <img src="${hero.icon}" alt="icon" />
+    `;
 
-    // slå dig løs her... 
+  services.forEach((service) => {
+    document.querySelector(".services").innerHTML += `
+        <div class="service">
+        <img src="${service.illustration}" alt="illustration" />
+        <h2>${service.headline}</h2>
+        <p>${service.text}</p>
+        <a href="#">${service.linktext}</a>
+        </div>
+        `;
+  });
 
-    //eksempel på at udskrive alle overskrifter i services i konsollen:
-    services.forEach(service => console.log(service.headline))
-
-}) // DOMContentLoaded slut
+  //eksempel på at udskrive alle overskrifter i services i konsollen:
+  services.forEach((service) => console.log(service.headline));
+}); // DOMContentLoaded slut
